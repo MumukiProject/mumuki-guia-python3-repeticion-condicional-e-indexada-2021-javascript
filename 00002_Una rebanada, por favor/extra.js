f879056360f1e36a035e52198bf877815d213db1
mayra@mumuki.org <mayra@mumuki.org>
2022-04-29 17:22:14 +0000

    Mumuki Export on 2022-04-29 17:22:14 +0000

--- 00002_Una rebanada, por favor/extra.js
+++ 00002_Una rebanada, por favor/extra.js
@@ -2,11 +2,11 @@ def listo():
   pass
 
 
-primera_estrofa = """
+let primeraEstrofa = /*
 Aquí me pongo a cantar		
 al compás de la vigüela,		
 que el hombre que lo desvela		
 una pena estraordinaria,		
 como la ave solitaria	
 con el cantar se consuela
-""".strip()
+*/.trim()
