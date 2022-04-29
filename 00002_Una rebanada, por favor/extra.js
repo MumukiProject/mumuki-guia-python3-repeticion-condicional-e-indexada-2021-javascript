918da202684a38b88f8ce98f7d2552da7556126d
mayra@mumuki.org <mayra@mumuki.org>
2022-04-29 17:24:57 +0000

    Mumuki Export on 2022-04-29 17:24:57 +0000

--- 00002_Una rebanada, por favor/extra.js
+++ 00002_Una rebanada, por favor/extra.js
@@ -1,6 +1,2 @@
-def listo():
-  pass
-
-
 let primeraEstrofa = 
 "Aquí me pongo a cantar /n al compás de la vigüela, /n que el hombre que lo desvela	/n una pena estraordinaria,	/n como la ave solitaria /n con el cantar se consuela".trim()
