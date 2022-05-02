2840712ab2a92d8dba97d505562c30563f653c76
mayra@mumuki.org <mayra@mumuki.org>
2022-05-02 18:35:17 +0000

    Mumuki Export on 2022-05-02 18:35:17 +0000

--- 00006_Todo suma/test..js
+++ 00006_Todo suma/test..js
@@ -1,19 +1,21 @@
-class Test(unittest.TestCase):
-
-  def test_productoria_de_10_2_3_es_60(self):
-    self.assertEquals(productoria([10, 2, 3]), 60)
-
-  def test_productoria_de_3_3_2_4_es_72(self):
-    self.assertEquals(productoria([3, 3, 2, 4]), 72)
-
-  def test_productoria_de_8_es_8(self):
-    self.assertEquals(productoria([8]), 8)
 
   def test_productoria_de_1_a_9_es_362880(self):
     self.assertEquals(productoria(range(1, 10)), 362880)
 
-  def test_productoria_de_6_es_6(self):
-    self.assertEquals(productoria([6]), 6)
-
-  def test_productoria_de_lista_vacia_es_1(self):
-    self.assertEquals(productoria([]), 1)
+describe("", () => {
+  it("productoria de la lista [10, 2, 3] es 60", () => {
+    assert.deepEqual(productoria([10, 2, 3]), 60);
+  })
+  it("productoria de la lista [3, 3, 2, 4] es 72", () => {
+    assert.deepEqual(productoria([3, 3, 2, 4]), 72);
+  })
+  it("productoria de la lista [8] es 8", () => {
+    assert.deepEqual(productoria([8]), 8);
+  })
+  it("productoria de la lista [6] es 6", () => {
+    assert.deepEqual(productoria([6]), 6);
+  })
+  it("productoria de la lista vacía es 1", () => {
+    assert.deepEqual(productoria([]), 1);
+  })
+})
