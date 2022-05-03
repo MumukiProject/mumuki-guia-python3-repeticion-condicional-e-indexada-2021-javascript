2c0b427f2125a3064e8318c56ba287f61ae58a55
gustavo@mumuki.org <gustavo@mumuki.org>
2022-05-03 18:28:56 +0000

    Mumuki Export on 2022-05-03 18:28:56 +0000

--- 00016_La fama es puro cuento/test..js
+++ 00016_La fama es puro cuento/test..js
@@ -1,10 +1,10 @@
 describe("", () => {
   it("cuantas veces entrenó lo suficiente con [35, 40, 32, 60] es 4", () => {
-    assert.deepEqual(cuantasVecesEntrenoLoSuficiente([35, 40, 32, 60])), 4);
+    assert.deepEqual(cuantasVecesEntrenoLoSuficiente([35, 40, 32, 60]), 4);
   })
   
   it("cuantas veces entrenó lo suficiente con [15, 45, 90, 0]", () => {
-    assert.deepEqual(cuantasVecesEntrenoLoSuficiente([15, 45, 90, 0])), 2);
+    assert.deepEqual(cuantasVecesEntrenoLoSuficiente([15, 45, 90, 0]), 2);
   })
 })  
     
