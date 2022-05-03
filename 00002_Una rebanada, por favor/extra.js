40fd89769185a2fca5d318247f80ceb57eb78c32
gustavo@mumuki.org <gustavo@mumuki.org>
2022-05-03 15:51:31 +0000

    Mumuki Export on 2022-05-03 15:51:31 +0000

--- 00002_Una rebanada, por favor/extra.js
+++ 00002_Una rebanada, por favor/extra.js
@@ -1,8 +1,8 @@
-let primeraEstrofa = `Aquí me pongo a cantar		
-al compás de la vigüela,		
-que el hombre que lo desvela		
-una pena estraordinaria,		
-como la ave solitaria	
+let primeraEstrofa = `Aquí me pongo a cantar
+al compás de la vigüela,
+que el hombre que lo desvela
+una pena estraordinaria,
+como la ave solitaria
 con el cantar se consuela`
 
 
