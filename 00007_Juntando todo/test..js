ef3d2e1951f8246fe33787a1a260709ac446afc2
mayra@mumuki.org <mayra@mumuki.org>
2022-05-03 15:00:25 +0000

    Mumuki Export on 2022-05-03 15:00:25 +0000

--- 00007_Juntando todo/test..js
+++ 00007_Juntando todo/test..js
@@ -9,3 +9,21 @@ class Test(unittest.TestCase):
   
   def test_juntar_cuatri_motor_devuelve_cuatrimotor(self):
     self.assertEqual(juntar(["cuatri", "motor"]), "cuatrimotor")
+    
+describe("", () => {
+  it("productoria de la lista [10, 2, 3] es 60", () => {
+    assert.deepEqual(productoria([10, 2, 3]), 60);
+  })
+  it("productoria de la lista [3, 3, 2, 4] es 72", () => {
+    assert.deepEqual(productoria([3, 3, 2, 4]), 72);
+  })
+  it("productoria de la lista [8] es 8", () => {
+    assert.deepEqual(productoria([8]), 8);
+  })
+  it("productoria de la lista [6] es 6", () => {
+    assert.deepEqual(productoria([6]), 6);
+  })
+  it("productoria de la lista vacía es 1", () => {
+    assert.deepEqual(productoria([]), 1);
+  })
+})
