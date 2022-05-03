310527bf374293f111218c07c7bd5b0f510774a2
mayra@mumuki.org <mayra@mumuki.org>
2022-05-03 15:10:57 +0000

    Mumuki Export on 2022-05-03 15:10:57 +0000

--- 00009_Todos los días un poco/default.js
+++ 00009_Todos los días un poco/default.js
@@ -1,7 +1,8 @@
-def todos_los_dias_un_poco(duraciones): 
+function todosLosDiasUnPoco(duraciones){ 
   ???
-  
-  for duracion in duraciones:
-    ???
-  
-  return ???
+
+  for (let duracion of duraciones){
+      ???
+  }
+  return ???;
+}
