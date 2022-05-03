1a17a4c743b4a9bf022e0b2334391a449a3ffce6
mayra@mumuki.org <mayra@mumuki.org>
2022-05-03 14:20:04 +0000

    Mumuki Export on 2022-05-03 14:20:04 +0000

--- 00009_Todos los días un poco/test..js
+++ 00009_Todos los días un poco/test..js
@@ -1,8 +1,8 @@
-class Test(unittest.TestCase):
-
-  def test_todos_los_dias_un_poco_con_35_40_32_60_es_True(self):
-    self.assertTrue(todos_los_dias_un_poco([35, 40, 32, 60]))
-    
-  def test_todos_los_dias_un_poco_con_15_45_90_0_es_False(self):
-    self.assertFalse(todos_los_dias_un_poco([15, 45, 90, 0]))
-    
+describe("", () => {
+  it("todos los días un poco con [35, 40, 32, 60] es true", () => {
+    assert(productoria([35, 40, 32, 60]));
+  })
+  it("todos los días un poco con [15, 45, 90, 0] es false", () => {
+    assert(productoria([15, 45, 90, 0]));
+  })
+})
