dee3548f3d33f7b0ed8bb3527580a45f41e43f09
mayra@mumuki.org <mayra@mumuki.org>
2022-05-03 15:13:05 +0000

    Mumuki Export on 2022-05-03 15:13:05 +0000

--- 00009_Todos los días un poco/test..js
+++ 00009_Todos los días un poco/test..js
@@ -1,8 +1,8 @@
 describe("", () => {
   it("todos los días un poco con [35, 40, 32, 60] es true", () => {
-    assert(productoria([35, 40, 32, 60]));
+    assert(todosLosDiasUnPoco([35, 40, 32, 60]));
   })
   it("todos los días un poco con [15, 45, 90, 0] es false", () => {
-    assert(productoria([15, 45, 90, 0]));
+    assert(todosLosDiasUnPoco([15, 45, 90, 0]));
   })
 })
